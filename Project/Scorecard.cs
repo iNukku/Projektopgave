76af9360cc873b447d0661625f3d8239795244ff
Bertil Andersen <bertilandersen@hotmail.com>
2016-12-05 01:40:02 +0100

    adjustments

--- Project/Scorecard.cs
+++ Project/Scorecard.cs
@@ -9,13 +9,17 @@ namespace Project
     public class Scorecard
     {
         #region fields and properties
-        private Dictionary<string, int> scores = new Dictionary<string, int>();
-        private Dictionary<string, bool> scoresAreUsed = new Dictionary<string, bool>();
+        //private Dictionary<string, int> scores = new Dictionary<string, int>();
+        //private Dictionary<string, bool> scoresAreUsed = new Dictionary<string, bool>();
+
         public int SumOfSingleValues { get; private set; }
         public int SumOfCombinedValues { get; private set; }
         public int Bonus { get; private set; }
         public int TotalSum { get; private set; }
         private bool allCombinationsUsed;
+        private int[] scores;
+        private bool[] scoresAreUsed;
+        private const int COMBINATIONS = 15;
 
         #endregion
 
@@ -26,97 +30,105 @@ namespace Project
             SumOfSingleValues = 0;
             Bonus = 0;
             TotalSum = 0;
+            scores = new int[COMBINATIONS];
+            scoresAreUsed = new bool[COMBINATIONS];
 
-            scores["ones"] = 0;
-            scores["twos"] = 0;
-            scores["threes"] = 0;
-            scores["fours"] = 0;
-            scores["fives"] = 0;
-            scores["sixes"] = 0;
-            scores["one Pair"] = 0;
-            scores["two pair"] = 0;
-            scores["three of Kind"] = 0;
-            scores["four of kind"] = 0;
-            scores["little straight"] = 0;
-            scores["big straight"] = 0;
-            scores["full house"] = 0;
-            scores["chance"] = 0;
-            scores["yatzy"] = 0;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                scores[i] = 0;
+            }
 
-            scoresAreUsed["ones"] = false;
-            scoresAreUsed["twos"] = false;
-            scoresAreUsed["threes"] = false;
-            scoresAreUsed["fours"] = false;
-            scoresAreUsed["fives"] = false;
-            scoresAreUsed["sixes"] = false;
-            scoresAreUsed["one pair"] = false;
-            scoresAreUsed["two pair"] = false;
-            scoresAreUsed["three of kind"] = false;
-            scoresAreUsed["four of kind"] = false;
-            scoresAreUsed["little straight"] = false;
-            scoresAreUsed["big straight"] = false;
-            scoresAreUsed["full house"] = false;
-            scoresAreUsed["chance"] = false;
-            scoresAreUsed["yatzy"] = false;
+            for (int i = 0; i < scoresAreUsed.Length; i++)
+            {
+                scoresAreUsed[i] = false;
+            }
+
+            //scores["ones"] = 0;
+            //scores["twos"] = 0;
+            //scores["threes"] = 0;
+            //scores["fours"] = 0;
+            //scores["fives"] = 0;
+            //scores["sixes"] = 0;
+            //scores["one Pair"] = 0;
+            //scores["two pair"] = 0;
+            //scores["three of Kind"] = 0;
+            //scores["four of kind"] = 0;
+            //scores["little straight"] = 0;
+            //scores["big straight"] = 0;
+            //scores["full house"] = 0;
+            //scores["chance"] = 0;
+            //scores["yatzy"] = 0;
+
+            //scoresAreUsed["ones"] = false;
+            //scoresAreUsed["twos"] = false;
+            //scoresAreUsed["threes"] = false;
+            //scoresAreUsed["fours"] = false;
+            //scoresAreUsed["fives"] = false;
+            //scoresAreUsed["sixes"] = false;
+            //scoresAreUsed["one pair"] = false;
+            //scoresAreUsed["two pair"] = false;
+            //scoresAreUsed["three of kind"] = false;
+            //scoresAreUsed["four of kind"] = false;
+            //scoresAreUsed["little straight"] = false;
+            //scoresAreUsed["big straight"] = false;
+            //scoresAreUsed["full house"] = false;
+            //scoresAreUsed["chance"] = false;
+            //scoresAreUsed["yatzy"] = false;
         }
         #endregion
 
         #region methods
+
         //private void setAllCombinationsUsed()
         //{
-        //    //Check at alle værdier er sat
+        //    int count = 0;
+        //    foreach (var value in scoresAreUsed.Values)
+        //    {
+        //        if (value == false)
+        //        {
+        //            count += 1;
+        //        }
+        //    }
+
+        //    if (count == 0)
+        //    {
+        //        allCombinationsUsed = true;
+        //    }
+        //    else
+        //    {
+        //        allCombinationsUsed = false;
+        //    }
+            
         //}
 
-        private void setAllCombinationsUsed()
-        {
-            int count = 0;
-            foreach (var value in scoresAreUsed.Values)
-            {
-                if (value == false)
-                {
-                    count += 1;
-                }
-            }
+        //private void setSumOfSingleValues()
+        //{
+        //    SumOfSingleValues = scores["ones"] + scores["twos"] + scores["threes"] + scores["fours"] + scores["fives"] + scores["sixes"];
+        //}
 
-            if (count == 0)
-            {
-                allCombinationsUsed = true;
-            }
-            else
-            {
-                allCombinationsUsed = false;
-            }
-            
-        }
+        //private void setSumOfCombinedValues()
+        //{
+        //    SumOfCombinedValues = scores["one pair"] + scores["two pair"] + scores["three of kind"] + scores["four of kind"] + scores["little straight"] +
+        //        scores["big straight"] + scores["full house"] + scores["chance"] + scores["yatzy"];
+        //}
 
-        private void setSumOfSingleValues()
-        {
-            SumOfSingleValues = scores["ones"] + scores["twos"] + scores["threes"] + scores["fours"] + scores["fives"] + scores["sixes"];
-        }
+        //private void setTotalSum()
+        //{
+        //    TotalSum = 0;
 
-        private void setSumOfCombinedValues()
-        {
-            SumOfCombinedValues = scores["one pair"] + scores["two pair"] + scores["three of kind"] + scores["four of kind"] + scores["little straight"] +
-                scores["big straight"] + scores["full house"] + scores["chance"] + scores["yatzy"];
-        }
+        //    TotalSum += Bonus;
 
-        private void setTotalSum()
-        {
-            TotalSum = 0;
+        //    foreach (var value in scores.Values)
+        //    {
+        //        TotalSum += value;
+        //    }
+        //}
 
-            TotalSum += Bonus;
-
-            foreach (var value in scores.Values)
-            {
-                TotalSum += value;
-            }
-        }
-
-        public bool ScoreCardIsFull()
-        {
-            setAllCombinationsUsed();
-            return allCombinationsUsed;
-        }
+        //public bool ScoreCardIsFull()
+        //{
+        //    setAllCombinationsUsed();
+        //    return allCombinationsUsed;
+        //}
         #endregion
     }
 }
